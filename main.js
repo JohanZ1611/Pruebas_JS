4cee8348d27b7f3742eaaa9c0b9f919a91b4ac9d
Johan Zuluaga <johan16zulu@gmail.com>
2023-05-29 11:02:16 -0500

    intro

---
+++ main.js
@@ -0,0 +1,14 @@
+//alertas
+// alert("hola soy johan");
+
+//variables
+let nombre = "Johan Zuluaga"; //*MEJOR RENDIMIENTO
+nombre = "Alan Smith";
+
+//Constantes
+const altura = 187;
+// altura = 186;
+
+//Mostrar en consola
+console.log(nombre);
+console.log(altura);
