51ca25630de681787f8a1c8e6d42808deb8b1703
Johan Zuluaga <johan16zulu@gmail.com>
2023-05-30 11:49:17 -0500

    enunciado ejercicio_1

---
+++ Ejercicio_1.js
@@ -0,0 +1,25 @@
+/*
+
+Enunciado Ejercicio 1:
+
+Dado un array de superheroes de Marvel, haz una programa que pueda 
+mostrar la informacion de un superheroe.
+
+y añade una capacidad de buscar la informacion de varios superheroes a la vez.
+
+
+Ejemplo: 
+mostrarInformacionSuperheroe('Iron Man');
+
+salida:
+Nombre real: Tony Stark
+Poderes: Tecnologia avanzada, Movilidad aerea
+Equipo: Los vengadores
+
+Ejemplo 2:
+mostrarInformacionSuperheroe([array de nombres]);
+
+Salida: 
+Muestra la informacion de todos los superheroes
+
+*/
