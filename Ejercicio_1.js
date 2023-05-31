af7b50947a579a515aa00774044b6c372c33656c
Johan Zuluaga <johan16zulu@gmail.com>
2023-05-31 10:50:37 -0500

    solucion de ejercicio_1

--- Ejercicio_1.js
+++ Ejercicio_1.js
@@ -23,3 +23,50 @@ Salida:
 Muestra la informacion de todos los superheroes
 
 */
+
+//*Solucion
+
+let superheroes = [
+  {
+    spiderman: {
+      nombreReal: "Peter Parker",
+      poderes: ["trepar paredes", "sentido arácnido", "lanzar telarañas"],
+      equipo: "Avengers",
+    },
+  },
+  {
+    batman: {
+      nombreReal: "Bruce Wayne",
+      poderes: ["inteligencia", "artes marciales", "tecnología"],
+      equipo: "Justice League",
+    },
+  },
+  {
+    wonderWoman: {
+      nombreReal: "Diana Prince",
+      poderes: ["fuerza", "velocidad", "lazo de la verdad"],
+      equipo: "Justice League",
+    },
+  },
+];
+
+// console.log(superheroes);
+
+const mostrarInformacionSuperheroe = (nombreHeroes) => {
+  if (typeof nombreHeroes === "string") {
+    let heroe = superheroes.find((obj) => obj[nombreHeroes]);
+    if (heroe) {
+      return heroe[nombreHeroes];
+    } else {
+      return "no se encontro siperheroe por ese nombre";
+    }
+  } else if (Array.isArray(nombreHeroes)) {
+    for (let heroe of nombreHeroes) {
+      console.log(mostrarInformacionSuperheroe(heroe));
+    }
+  } else {
+    return " el parqmetro debe de ser un string o un array";
+  }
+};
+
+mostrarInformacionSuperheroe(["batman", "wonderWoman"]);
